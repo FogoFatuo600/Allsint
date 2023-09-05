7e224e619b68f96bef9a94683190e7be7a82f231
FogoFatuo600 <vascoeselecaooo@gmail.com>
2023-09-05 21:07:47 +0000

    Add Menu

---
+++ src/Menu.py
@@ -0,0 +1,14 @@
+ASCII_ART = r"""░█████╗░██╗░░░░░██╗░░░░░░██████╗██╗███╗░░██╗████████╗
+██╔══██╗██║░░░░░██║░░░░░██╔════╝██║████╗░██║╚══██╔══╝
+███████║██║░░░░░██║░░░░░╚█████╗░██║██╔██╗██║░░░██║░░░
+██╔══██║██║░░░░░██║░░░░░░╚═══██╗██║██║╚████║░░░██║░░░
+██║░░██║███████╗███████╗██████╔╝██║██║░╚███║░░░██║░░░
+╚═╝░░╚═╝╚══════╝╚══════╝╚═════╝░╚═╝╚═╝░░╚══╝░░░╚═╝░░░"""
+
+DESCRIPTION = r"""Commands search data
+
+allsint -name <Name>                 Search by real names
+allsint -nickname <Nickname>         Search by nicknames
+allsint -email <email@example.com>   Search by email
+allsint -number <+1 408 000 0000>    Search by numbers
+allsint -img <dir/example/img.jpg>   Search by image"""
